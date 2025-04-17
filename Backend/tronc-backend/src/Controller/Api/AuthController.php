1f9fe6ed3d0cc20a85ea5e0cf06dc73a730f782d
LordMomonga <kamisisamuel28@gmail.com>
2025-04-17 06:10:58 -0400

    Ajout du Backends

---
+++ Backend/tronc-backend/src/Controller/Api/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AuthController extends AbstractController
+{
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function register(Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setName($data['name']);
+
+        $hashedPassword = $hasher->hashPassword($user, $data['password']);
+        $user->setPassword($hashedPassword);
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse(['message' => 'User registered successfully'], 201);
+    }
+}
